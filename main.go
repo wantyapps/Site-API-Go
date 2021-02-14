43d7f57a0bde4f4d8aa20446845ece43030b708c
Uri Arev <uri.arev@gmail.com>
2021-02-14 12:33:51 +0200

    Adding a gooder user interface

--- main.go
+++ main.go
@@ -10,19 +10,22 @@ import (
 func main() {
 	client := &http.Client{}
 	req, err := http.NewRequest("GET", "http://localhost/api", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
 	req.Header.Set("password", "PASSWORD")
 	req.Header.Set("username", "TEST")
 	res, err := client.Do(req)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println("[\033[91m!\033[0m] Connection error.")
+		return
 	}
 	defer res.Body.Close()
-	body, err := ioutil.ReadAll(res.Body)
+	body, _ := ioutil.ReadAll(res.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(string(body))
+
+	if string(body) == "{\"success\": true}" {
+		fmt.Println("[\033[92m+\033[0m] " + string(body))
+	} else {
+		fmt.Println("[\033[91m!\033[0m] " + string(body))
+	}
 }
